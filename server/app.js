b135c37544b28cbb581881c0dbcecb345f55c563
Cadeniuc <icadeniuc16@gmail.com>
2020-05-22 21:53:48 +0300

    Роуты, контроллеры, модели

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const autoRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
 
+mongoose.connect(keys.MONGO_URI)
+  .then(() => console.log('Mongo connected ...'))
+  .catch(error => console.error(error))
+
+
+
+
 app.use(bodyParser.urlencoded({extented: true}))
-app.use(bodyParser.json() )
+app.use(bodyParser.json())
+
+
+app.use('/api/auth', autoRoutes)
 
 module.exports = app
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-
+  MONGO_URI: 'mongodb+srv://usernuxt:Chisinau0124@nuxt-base-9klos.mongodb.net/test?retryWrites=true&w=majority'
 }
